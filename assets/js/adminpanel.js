dd0e6ba1bc9ee2414c62ff1de17b6731b5f4e93f
CryptonFuture <crypton.futuremedia1989@gmail.com>
2025-07-03 10:42:37 +0500

    logout api is integrated

--- assets/js/adminpanel.js
+++ assets/js/adminpanel.js
@@ -24,3 +24,45 @@
 
 })(jQuery);
 
+const prefix = 'api/v1'
+const baseUrl = `http://localhost:8000/${prefix}`
+
+async function logout() {
+		const userId = localStorage.getItem('user')
+
+		const res = await fetch(`${baseUrl}/logout?id=${userId}`, {
+			method: 'POST'
+		})
+
+		const data = await res.json()
+
+		if (res.ok) {
+			localStorage.removeItem('token');
+			localStorage.removeItem('user');
+			localStorage.removeItem('email');
+			localStorage.removeItem('tokenType');
+			localStorage.removeItem('rememberMe')
+			localStorage.removeItem('rememberedEmail');
+      		localStorage.removeItem('rememberedPassword');
+
+			Swal.fire({
+				icon: 'success',
+				title: 'Logout Successful',
+				text: data.message,
+				timer: 2000,
+				showConfirmButton: false,
+				timerProgressBar: true
+			}).then(() => {
+				window.location.href = '/';
+
+			});
+
+		} else {
+			Swal.fire({
+				icon: 'error',
+				title: 'Logout Failed',
+				text: data.message
+			});
+		}
+	}
+
